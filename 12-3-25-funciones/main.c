c10776ef61a981c7b7aa52f5554c798f931297e7
Gonzalo Rochet <grochet@mobydigital.com>
2025-03-13 19:47:03 -0300

    add examples on funtions tp

--- 12-3-25-funciones/main.c
+++ 12-3-25-funciones/main.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 
 void sumarEdadesReferencia(int* num1, int* num2, int*resultado);
+void intercambio (int * n1, int * n2);
 
 int main()
 {
     printf("Hello world!\n");
+
+
+    /*
 
     int edadJuan = 12;
     int edadMaria = 15;
@@ -18,6 +22,41 @@ int main()
 
 
     sumarEdadesReferencia(&edadJuan, &edadMaria, &sumaEdadesAlumnos);
+
+
+
+    */
+
+    /*
+    int edadPedro = 15;
+
+    printf("\n Edad Pedro vive (ddm) en %p y almacena %d \n", &edadPedro, edadPedro);
+
+    int * pEdadPedro = &edadPedro;
+
+    printf("\n pEdadPedro vive (ddm) en %p y almacena %p \n", &pEdadPedro, pEdadPedro);
+
+    int ** pApEdadPedro = pEdadPedro;
+
+    printf("\n pApEdadPedro vive (ddm) en %p y almacena %p \n", &pApEdadPedro, pApEdadPedro);
+
+    *pApEdadPedro = 10;
+
+    printf("La edad de Pedro es %d", edadPedro);
+
+    */
+
+
+    int A = 5;
+    int B = 2;
+
+    printf("La variable A antes %d y la variable B antes %d \n" ,A, B);
+
+    intercambio(&A,&B);
+
+    printf("La variable A despues %d y la variable B despues %d \n" ,A, B);
+
+
 
     return 0;
 }
@@ -36,4 +75,16 @@ void sumarEdadesReferencia(int* num1, int* num2, int*resultado){
 
 
     *resultado = *num1 + *num2;
+}
+
+
+void intercambio (int * n1, int * n2){
+
+    // printf("\n\nAntes - N1: %d - N2 %d", n1, n2);
+
+    int aux = * n1;
+    *n1 = *n2;
+    *n2 = aux;
+
+   // printf("\n\nDespues - N1: %d - N2 %d \n\n", n1, n2);
 }
